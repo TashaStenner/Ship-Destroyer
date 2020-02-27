35214a229be907550900334d1e026098254301aa
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-02-27 12:45:29 +0000

    still-broke

--- console-log-tests.js
+++ console-log-tests.js
@@ -1,67 +1,67 @@
-class ship {
-    constructor(type, points, hitPoints){
-    this.type = type;
-    this.points = points;
-    this.hitPoints = hitPoints;
-    this.isAlive = true;
-}
-takesDamage(){
-    this.points = this.points - this.hitPoints;
-    this.points <= 0 ? 0 : this.points;
-    return this.points;
-}
+// class ship {
+//     constructor(type, points, hitPoints){
+//     this.type = type;
+//     this.points = points;
+//     this.hitPoints = hitPoints;
+//     this.isAlive = true;
+// }
+// takesDamage(){
+//     this.points = this.points - this.hitPoints;
+//     this.points <= 0 ? 0 : this.points;
+//     return this.points;
+// }
 
-    dies(){
-        return this.isAlive = false;
-    }
+//     dies(){
+//         return this.isAlive = false;
+//     }
     
-}
+// }
 
-class Mothership extends ship{
-constructor(type, points, hitPoints){
-    super(type, points, hitPoints);
+// class Mothership extends ship{
+// constructor(type, points, hitPoints){
+//     super(type, points, hitPoints);
     
-} 
+// } 
 
-allDie(){
-    alert("game ends");
-}
-}
+// allDie(){
+//     alert("game ends");
+// }
+// }
 
-const mothership = new Mothership ("mothership", 100 , 9);
-const defenseship = new ship ("defenseship" , 80 , 10);
-const attackship = new ship ("attackship" , 45 , 12);
+// const mothership = new Mothership ("mothership", 100 , 9);
+// const defenseship = new ship ("defenseship" , 80 , 10);
+// const attackship = new ship ("attackship" , 45 , 12);
 
-mothership.takesDamage()
-console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
 
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
 
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
 
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
-mothership.takesDamage()
-console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
+// mothership.takesDamage()
+// console.log(mothership.points);
